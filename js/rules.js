35a52fe1ef583b0e5512f56dd803d582eea24c8d
marcita <orozcomarcia3@gmail.com>
2024-07-23 20:06:35 -0300

    Agrego pagina de reglas y link a repositorio del proyecto

--- js/rules.js
+++ js/rules.js
@@ -1,11 +1,16 @@
-'use strict';
+"use strict";
 
-document.addEventListener('DOMContentLoaded', () => {
-    /**
-    * MENU RESPONSIVE
-    * 
-    */
-    var menuIcon = document.getElementById('menu-icon');
-    var navbarLinks = document.querySelector('.navbar-links');
-    menuIcon.addEventListener('click', () => navbarLinks.classList.toggle('active'));
+document.addEventListener("DOMContentLoaded", () => {
+  /**
+   * MENU RESPONSIVE
+   *
+   */
+  var menuIcon = document.getElementById("menu-icon");
+  var navbarLinks = document.querySelector(".navbar-links");
+  menuIcon.addEventListener("click", () =>
+    navbarLinks.classList.toggle("active")
+  );
+  var playerName = localStorage.getItem("playerName") || "Jugador";
+  var playerNameSpan = document.querySelector("#player-name");
+  playerNameSpan.textContent = playerName;
 });
